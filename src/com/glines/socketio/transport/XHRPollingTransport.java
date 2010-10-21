c05a7b9a09887e66fb0e40f81ff5ec9c15c2ade9
Tad Glines <tad.glines@gmail.com>
2010-10-21 07:19:56 -0700

    Add copyright notice to files. Added some CORS related headers in XHRPollingTransport.java

--- src/com/glines/socketio/transport/XHRPollingTransport.java
+++ src/com/glines/socketio/transport/XHRPollingTransport.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License
+ * Copyright (c) 2010 Tad Glines
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.glines.socketio.transport;
 
 import java.io.BufferedReader;
@@ -85,9 +108,7 @@ public class XHRPollingTransport extends AbstractHttpTransport {
 						HttpServletResponse response, SocketIOSession session)
 						throws IOException {
 					if ("GET".equals(request.getMethod())) {
-						if (isConnected == false) {
-							response.setHeader("Connection", "close");
-						} else {
+						if (isConnected != false) {
 							/*
 							 * If there are messages in the buffer, get them and return them.
 							 * If not, create continuation, register listener with buffer that will
@@ -128,6 +149,16 @@ public class XHRPollingTransport extends AbstractHttpTransport {
 								data = str.toString();
 							}
 							list = decode(data.substring(5));
+						}
+						String origin = request.getHeader("Origin");
+						if (origin != null) {
+							// TODO: Verify origin
+							
+							// TODO: Explain why this is needed. Possibly for IE's XDomainRequest?
+							response.setHeader("Access-Control-Allow-Origin", "*");
+							if (request.getCookies() != null) {
+								response.setHeader("Access-Control-Allow-Credentials", "true");
+							}
 						}
 						for (String msg: list) {
 							try {
