c3e1b4674fde3cbe355e700bc5d32ff04de73b15
Mathieu Carbou <kha@box.(none)>
2011-01-24 11:31:29 -0500

    Fixing parent pom to fix licensing error

--- samples/echo/src/main/java/com/glines/socketio/examples/echo/EchoSocketServlet.java
+++ samples/echo/src/main/java/com/glines/socketio/examples/echo/EchoSocketServlet.java
@@ -1,6 +1,6 @@
 /**
  * The MIT License
- * Copyright (c) 2010 Tad Glines, Mathieu Carbou, Ovea
+ * Copyright (c) 2010 Tad Glines
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
